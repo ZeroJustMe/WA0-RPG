319e561d8cfd73d89683882773f66c0ea22dbe19
ROROBINBIN <464214931@qq.com>
2018-03-01 00:00:39 +0800

    FIX2

---
+++ Assets/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    GameObject tmp;
+
+    void PaintBackGround()
+    {
+        GameObject BackGround = Instantiate(Resources.Load("Prefebs/Map/Novice Village1") as GameObject) as GameObject;
+
+    }
+
+	// Use this for initialization
+	void Start () {
+        Destroy(GameObject.Find("Canvas"));
+        tmp = GameObject.Find("Main Camera");
+        PaintBackGround();
+        //PaintUI();
+        //PaintState();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Resources/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    void PaintBackGround()
+    {
+        GameObject BackGround = Instantiate(Resources.Load("Prefebs/Map/Novice Village1") as GameObject) as GameObject;
+
+    }
+
+	// Use this for initialization
+	void Start () {
+        Destroy(GameObject.Find("Canvas"));
+        PaintBackGround();
+        //PaintUI();
+        //PaintState();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
